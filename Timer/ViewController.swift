6f6418ef4e24a9e923fb37e5706d4308de7c51f0
Guilherme Crozariol <guilherme@guilhermes-macbook-pro.local>
2016-12-16 13:59:47 -0200

    Timer.

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -10,6 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = Timer()
+    
+    var time = 200
+    
+    func processTimer() {
+        if time > 0 {
+            time -= 1
+            timerLabel.text = String(time)
+        } else {
+            timer.invalidate()
+        }
+    }
+    
+    @IBAction func play(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
+    @IBAction func pause(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    @IBAction func minusTen(_ sender: Any) {
+        time -= 10
+    }
+    
+    @IBAction func plusTen(_ sender: Any) {
+        time += 10
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        time = 201
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +57,3 @@ class ViewController: UIViewController {
 
 
 }
-
